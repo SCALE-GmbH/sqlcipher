b98816592e03c760e6117d9e1ab820376beec5d5
Raul Rosenlöcher <raul.rosenloecher@scale.eu>
2019-01-31 14:46:34 +0100

    decouple env setting and build

--- scale/scale_build.sh
+++ scale/scale_build.sh
@@ -4,8 +4,6 @@ set -eo pipefail
 ## go to base dir of sqlcipher, not to this subdir!
 #cd $(dirname `readlink -f "$0"`)/..
 
-source ./scale/scale_include.sh
-
 echo -e '\nbuild'
 ./configure --verbose --enable-tempstore=yes --with-crypto-lib=$CRYPTO_LIB $DEBUG_FLAGS CFLAGS="$CFLAGS"
 make clean
